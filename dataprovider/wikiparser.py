107b02ad5ef2fef04bc89210c5e4367215e24b9b
ZaurBegiev <whereistheslim@gmail.com>
2022-05-21 20:52:19 +0300

    Added logging and in-memory cache

--- dataprovider/wikiparser.py
+++ dataprovider/wikiparser.py
@@ -1,11 +1,15 @@
 from datetime import datetime
+import logging
 import requests
 import time
 
 from bs4 import BeautifulSoup
 
+logging.basicConfig(level=logging.DEBUG)
+
 MAIN = 'https://en.wikipedia.org'
 RECENT_CHANGES_PAGE_URL = 'https://en.wikipedia.org/wiki/Special:RecentChanges'
+CACHE_SIZE_THRESHOLD = 500
 
 
 def refresh_recent_changes():
@@ -19,6 +23,13 @@ def get_url_suffix(change):
     return change.find('a', {'class': 'mw-changeslist-diff'}).get('href')
 
 
+def extract_diff_id(url_suffix):
+    first_split = url_suffix.split('&')
+    second_split = [_.split('=') for _ in first_split]
+    split_to_dict = {key: value for key, value in second_split}
+    return int(split_to_dict['diff'])
+
+
 def parse_change_url(change):
     if not change.get('data-mw-logaction'):
         url_suffix = get_url_suffix(change)
@@ -28,14 +39,16 @@ def parse_change_url(change):
     ts = change.get('data-mw-ts')
     result_message = {
         'timestamp': datetime.strptime(ts, '%Y%m%d%H%M%S').strftime('%Y-%m-%d %H:%M:%S'),
-        'url_suffix': url_suffix
+        'url_suffix': url_suffix,
+        'diff_id': extract_diff_id(url_suffix),
     }
     return result_message
 
 
 def run():
+    cache = set()
+
     while True:
-        print('-' * 100)
         time.sleep(2)
         recent_changes = refresh_recent_changes()
         for change in recent_changes:
@@ -43,9 +56,17 @@ def run():
                 if change:
                     message = parse_change_url(change)
                     if message:
-                        print(message)
+                        diff_id = message['diff_id']
+                        if diff_id in cache:
+                            pass
+                        else:
+                            cache.add(message['diff_id'])
+                            logging.info(message)
             except AttributeError:
                 pass
+        if len(cache) > CACHE_SIZE_THRESHOLD:
+            cache = set(list(sorted(cache))[len(cache) - CACHE_SIZE_THRESHOLD:])
+            logging.info('Obsolete cache cleared')
 
 
 if __name__ == '__main__':
